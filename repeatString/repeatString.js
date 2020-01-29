70ab6333619cbf50bd00070a8d311631f9a77e10
Jonno <garbage.schemmel@gmail.com>
2020-01-28 19:46:08 -0600

    solved

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeat, freq) {
+    if(freq < 0)
+        return 'ERROR'
+    holder = '';
+    for(i = 0; i < freq; i++)
+    {
+        holder += repeat;
+    }
+    return holder;
 }
 
 module.exports = repeatString
