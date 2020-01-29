305824f8969fda7466bbda16ab033614685b8862
Jonno <garbage.schemmel@gmail.com>
2020-01-28 20:29:57 -0600

    solved

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (backwards) {
+    "use strict";
+    var arr = backwards.split('');
+    var bArr = '';
+    var i;
+    for(i = arr.length -1; i >= 0 ; i--) {
+        bArr += arr[i];
+    }
+    return bArr;
 
 }
 
-module.exports = reverseString
+module.exports = reverseString;
